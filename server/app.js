1fb98bfb5347e593d66c31583c7b2f768326fecb
ANN LIN <smilemarg@gmail.com>
2020-05-26 17:42:37 -0400

    deploy

--- server/app.js
+++ server/app.js
@@ -11,7 +11,9 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 
 // SESSION MIDDLEWARE
 app.use(session({
