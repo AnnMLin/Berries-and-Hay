82fbacd0ff3c65dc766c03709d8ef45a1bc061b2
ANN LIN <smilemarg@gmail.com>
2020-05-26 19:07:06 -0400

    deploy

--- server/app.js
+++ server/app.js
@@ -35,10 +35,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(passport.initialize())
-// app.use(passport.session())
+// PUBLIC PATH
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.use('/user', userRouter)
 app.use('/auth', authRouter)
