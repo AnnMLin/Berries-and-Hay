e246871fb2caeb1dc25b4265d1aa23e78b55fd8d
ANN LIN <smilemarg@gmail.com>
2020-05-27 00:09:12 -0400

    remove build from gitignore

---
+++ client/build/precache-manifest.a0e0142ddb943c412ee2706de9749612.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee263b771c73c10ea0f5d11355edb55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33e8d3992002077813e6",
+    "url": "/static/js/2.e0b76ecf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e0b76ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6fe5b6c9e9b5702f6fa",
+    "url": "/static/js/main.fe827836.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
